48d1c515fb198d6a40ea91d3a808e6b1ad29fe4b
unknown <caio.apmonteiro@gmail.com>
2023-08-07 21:00:04 -0300

    Métodos getter e setter implementados

--- teste.dart
+++ teste.dart
@@ -1,4 +1,14 @@
 class Teste {
   var estudante;
   var nota;
+
+  String getEstudante() => this.estudante;
+  void setEstudante(String e) {
+    this.estudante = e;
+  }
+
+  double getNota() => this.nota;
+  void setNota(double n) {
+    this.nota = n;
+  }
 }
